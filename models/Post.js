84bf534ac024a165ba6895524fc4f1af6cbbe795
Giovanni Charles <giovanni.charles@gmail.com>
2015-03-06 16:39:07 +0000

    embed soundcloud

--- models/Post.js
+++ models/Post.js
@@ -21,6 +21,7 @@ Post.add({
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
 		extended: { type: Types.Html, wysiwyg: true, height: 400 }
 	},
+  soundcloudEmbed: { type: String },
 	categories: { type: Types.Relationship, ref: 'PostCategory', many: true }
 });
 
